d97897bf68cad86578c8d1f7d15102ce3ad5489d
Saroni0504 <85100855+Saroni0504@users.noreply.github.com>
2024-08-07 17:39:57 +0300

    Update TwoSum.java

--- Java/TwoSum.java
+++ Java/TwoSum.java
@@ -29,13 +29,17 @@ Output: [0,1]
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++){
-            for (int j = 0; j < nums.length; j++){
+            for (int j = i + 1; j < nums.length; j++){
                 if (nums[i] + nums[j] == target && i != j){
                     int [] result = {i,j};
                     return result;
                 }
             }
         }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
         throw new IllegalArgumentException("No two sum solution");
     }
 }
